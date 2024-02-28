570c4f5e4a916ea3a3599e45431cdd75c805c4be
Divyam Choudhary <divyamchoudhary@hotmail.com>
2024-02-28 12:21:11 +0530

    ICVGIP 2024 initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+window.onscroll = function() {navBarScrollLock()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+console.log(navbar, sticky)
+
+function navBarScrollLock() {
+    console.log(window.scrollY)
+    if (window.scrollY >= sticky) {
+        console.log(navbar.classList)
+
+        navbar.classList.add("fixed");
+        navbar.classList.add("top-0");
+        navbar.classList.add("w-full");
+        navbar.classList.add("max-w-xl");
+        navbar.classList.add("lg:max-w-4xl");
+        navbar.classList.add("xl:max-w-6xl");
+        navbar.classList.add("2xl:max-w-7xl");
+        // navbar.classList.add("w-72rem");
+        // max-w-xl lg:max-w-4xl xl:max-w-6xl 2xl:max-w-7xl
+    } else {
+        navbar.classList.remove("fixed");
+        navbar.classList.remove("top-0");
+        navbar.classList.remove("w-full");
+        navbar.classList.remove("max-w-xl");
+        navbar.classList.remove("lg:max-w-4xl");
+        navbar.classList.remove("xl:max-w-6xl");
+        navbar.classList.remove("2xl:max-w-7xl");
+        // navbar.classList.remove("w-72rem");
+    }
+}
+
+const navham = document.getElementById("")
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'iiitb-cover-1' :  "url('/assets/2022.jpg')",
+        'iiitb-cover-2' :  "url('/assets/iiitb_cover2.jpg')",
+      },
+      spacing: {
+        '72rem': '72rem',
+      },
+      colors: {
+        'white': '#fff',
+        'navigationPurple': {
+          '50': '#eff4fe',
+          '100': '#e2eafd',
+          '200': '#cbd8fa',
+          '300': '#acbcf5',
+          '400': '#8a9aef',
+          '500': '#6e78e6',
+          '600': '#5354d8',
+          '700': '#4442be',
+          '800': '#39399a',
+          '900': '#35377a',
+          '950': '#1f1f47',
+        },
+      }
+    },
+    
+  },
+  plugins: [],
+}
